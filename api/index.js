b074e73d3555f6245190fcdbcc13ad55dd2e6f0f
jaiveer4020y-commits <jaiveer4020y@gmail.com>
2025-12-30 16:41:44 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -105,6 +105,3 @@ def home():
         'example': '/api/get-stream?title=wednesday.s01e02'
     })
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 10000))
-    app.run(host='0.0.0.0', port=port)
